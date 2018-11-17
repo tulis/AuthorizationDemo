328e254eb0bb3eba52a91a9bf44c7c3179ae9acc
tulis <tanri.suryati@outlook.com>
2018-11-18 02:18:39 +1100

    Restructure project into a solution

---
+++ AuthorizationDemo/IAuthorizer.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace AuthorizationDemo
+{
+    public interface IAuthorizer
+    {
+        Task Authorize();
+    }
+}
---
+++ AuthorizationDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthorizationDemo
+{
+    public static class Program
+    {
+
+        //https://auth0.com/docs/quickstart/backend/aspnet-core-webapi/01-authorization
+        public static async Task Main(string[] args)
+        {
+            var authorizer = new Auth0Authorizer();
+
+            await authorizer.Authorize();
+
+            Console.WriteLine();
+            Console.WriteLine("Press ENTER to continue...");
+            Console.ReadLine();
+        }
+    }
+}
